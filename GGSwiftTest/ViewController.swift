b050aa184404cef557c9d886a431d350c213e6d8
hq viet <hqviet@hqs-Mac-mini.local>
2014-09-30 17:21:38 +0700

    Initial Commit

---
+++ GGSwiftTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GGSwiftTest
+//
+//  Created by hq viet on 9/30/14.
+//  Copyright (c) 2014 __GG4aCrossover__. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
